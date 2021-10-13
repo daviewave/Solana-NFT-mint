14393208bf0c178cc2b226d85387388476191c28
David Silveira <daviewave@gmail.com>
2021-10-13 18:11:35 -0400

    instantiated a new connection to the solana devnet network

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 //variables to ensure the specified JS libraries are present
 var web3 = require("@solana/web3.js");
 var splToken = require("@solana/spl-token");
+
+//instantiating a new connection instance to the solana devnet
+async () => {
+  // Connect to cluster
+  var connection = new web3.Connection(
+    web3.clusterApiUrl("devnet"),
+    "confirmed"
+  );
+};
