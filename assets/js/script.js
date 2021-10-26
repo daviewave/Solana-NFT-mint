a0397f4e063120c871d46f16c3ae9aeeb1b02d0b
David Silveira <daviewave@gmail.com>
2021-10-26 14:38:13 -0400

    this was the one that added script.js and its framework

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+var connectBtn = document.getElementById("connect-wallet-button");
+
+//1.
+connectBtn.addEventListener("click", function () {
+  //function logic
+});
+
+//OR
+
+//2.
+//or can create a new function and call it in the event listener
+function connectWallet() {}
+// connectBtn.addEventListener("click", connectWallet);
