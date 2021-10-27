a3b3404efca02dee8c86d7b07549baa3a4fb5eab
David Silveira <daviewave@gmail.com>
2021-10-27 15:50:40 -0400

    added TODO list for functions needed in scrip.js + added 2nd column container background color to home-container

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,19 @@ function connectWallet_3() {
   //write code to connect to wallet here
 }
 
+/*TODO: WILL NEED FUNCTIONS TO CREATE AND ADD THE OTHER SECTIONS OF THE SITE AS THE USER CLICKS ON DIFFERENT OPTIONS TO NAV WITH NAVBARD*/
+//TODO(1): WRITE FUNCTION TO CREATE 'ABOUT' SECTION
+function createAboutSection() {}
+
+//TODO(2): WRITE FUNCTION TO CREATE 'RARITY' SECTION
+function createRaritySection() {}
+
+//TODO(3): WRITE FUNCTION TO CREATE 'ROADMAP' SECTION
+function createRoadmapSection() {}
+
+//TODO(4): WRITE FUNCTION TO CREATE 'FAQ' SECTION
+function createFaqSection() {}
+
 //USER INTERACTIONS
 connectPhantom.addEventListener("click", connectPhantomWallet);
 
