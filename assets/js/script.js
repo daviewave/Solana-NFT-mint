b080be728472b373f4330a376b1c17f84ad3a138
David Silveira <daviewave@gmail.com>
2021-10-27 19:20:55 -0400

    added variable to target main-container element in index.html and added a parameter for the functions that are building the sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,9 @@ var connectPhantom = document.getElementById("phantom-wallet");
 var connectWallet2 = document.getElementById("wallet-2");
 var connectWallet3 = document.getElementById("wallet-3");
 
+//will be able to append new sections to this variable after all other elements that need to go within that section are created
+var $targetMainContainer = $("#main-container");
+
 //FUNCTIONS
 function connectPhantomWallet() {
   //write code to connect to wallet here
@@ -18,16 +21,23 @@ function connectWallet_3() {
 
 /*TODO: WILL NEED FUNCTIONS TO CREATE AND ADD THE OTHER SECTIONS OF THE SITE AS THE USER CLICKS ON DIFFERENT OPTIONS TO NAV WITH NAVBARD*/
 //TODO(1): WRITE FUNCTION TO CREATE 'ABOUT' SECTION
-function createAboutSection() {}
+function createAboutSection($container) {
+  var $aboutContainerDiv = $("<div>");
+}
+
+//TODO(1.A): CREATE THE 'SUB-CONTAINER' DIV THAT WILL HOLD ALL OTHER CONTENTS
+var $aboutContainerDiv = $("<div>");
 
 //TODO(2): WRITE FUNCTION TO CREATE 'RARITY' SECTION
-function createRaritySection() {}
+function createRaritySection($container) {}
 
 //TODO(3): WRITE FUNCTION TO CREATE 'ROADMAP' SECTION
-function createRoadmapSection() {}
+function createRoadmapSection($container) {}
 
 //TODO(4): WRITE FUNCTION TO CREATE 'FAQ' SECTION
-function createFaqSection() {}
+function createFaqSection($container) {}
+
+//HELPER FUNCTIONS
 
 //USER INTERACTIONS
 connectPhantom.addEventListener("click", connectPhantomWallet);
